63e60e8b556d4c2a4cbe4912c6800335e7726d1f
PDDaniel <daniel@proper-dog.com>
2016-03-30 12:32:39 +0200

    Initial Commit

---
+++ MoveCursor/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MoveCursor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.Timer_MoveCursor = new System.Windows.Forms.Timer(this.components);
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(63, 40);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(146, 87);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Timer_MoveCursor
+            // 
+            this.Timer_MoveCursor.Interval = 1;
+            this.Timer_MoveCursor.Tick += new System.EventHandler(this.OnMoveCursorTick);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(94, 142);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(75, 23);
+            this.btnUp.TabIndex = 1;
+            this.btnUp.Text = "Up";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(22, 171);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 2;
+            this.btnLeft.Text = "Left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(169, 171);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 3;
+            this.btnRight.Text = "Right";
+            this.btnRight.UseVisualStyleBackColor = true;
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(94, 200);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 23);
+            this.btnDown.TabIndex = 4;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer Timer_MoveCursor;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnDown;
+    }
+}
+
---
+++ MoveCursor/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoveCursor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int counter = 0;
+        private void OnMoveCursorTick(object sender, EventArgs e)
+        {
+            switch (counter)
+            {
+                case 0:
+                    MoveMouse(new Point(0, -5)); // Up
+                    break;
+                case 1:
+                    MoveMouse(new Point(5, 0)); // Right
+                    break;
+                case 2:
+                    MoveMouse(new Point(0, 5)); // Down
+                    break;
+                case 3:
+                    MoveMouse(new Point(-5, 0)); // Left
+                    break;
+                default:
+                    break;
+            }
+
+            if (counter < 3)
+            {
+                counter++;
+            }
+            else
+            {
+                counter = 0;
+            }
+
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            Point currentCurserPos = Cursor.Position;
+            Point up = new Point(0, -15);
+            Cursor.Position = new Point(currentCurserPos.X + up.X, currentCurserPos.Y + up.Y);
+        }
+
+        private void MoveMouse(Point direction)
+        {
+            Point currentCurserPos = Cursor.Position;
+            Cursor.Position = new Point(currentCurserPos.X + direction.X, currentCurserPos.Y + direction.Y);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Timer_MoveCursor.Start();
+        }
+    }
+}
